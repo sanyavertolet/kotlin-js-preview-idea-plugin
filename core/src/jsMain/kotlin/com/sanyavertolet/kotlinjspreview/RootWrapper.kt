d9824e5578103be68ea0c08da59f434794253239
sanyavertolet <lxnfrolov@gmail.com>
2023-09-24 01:25:56 +0400

    Settings disclaimer
    ### What's done:
     * Added settings disclaimer - work is still in progress
     * Removed useless Wrapper.kt
     * Removed `@MustBeDocumented` annotation from `RootWrapper` annotation

--- core/src/jsMain/kotlin/com/sanyavertolet/kotlinjspreview/RootWrapper.kt
+++ core/src/jsMain/kotlin/com/sanyavertolet/kotlinjspreview/RootWrapper.kt
@@ -3,5 +3,4 @@ package com.sanyavertolet.kotlinjspreview
 @Suppress("unused")
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.SOURCE)
-@MustBeDocumented
 annotation class RootWrapper

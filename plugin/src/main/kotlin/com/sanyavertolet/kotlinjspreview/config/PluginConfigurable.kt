198caafa510dd3db97b9732e25f08e798c20efe3
sanyavertolet <lxnfrolov@gmail.com>
2023-09-28 17:21:29 +0400

    Better settings UI
    ### What's done:
     * Fixed a bug with List and ListModel sync
     * Improved settings UI

--- plugin/src/main/kotlin/com/sanyavertolet/kotlinjspreview/config/PluginConfigurable.kt
+++ plugin/src/main/kotlin/com/sanyavertolet/kotlinjspreview/config/PluginConfigurable.kt
@@ -1,16 +1,12 @@
 package com.sanyavertolet.kotlinjspreview.config
 
 import com.intellij.openapi.options.Configurable
-import com.intellij.ui.components.JBList
-import com.intellij.ui.components.JBScrollPane
-import com.intellij.ui.components.JBTextArea
-import com.intellij.util.ui.JBInsets
+import com.intellij.ui.JBColor
+import com.intellij.ui.TitledSeparator
+import com.intellij.ui.components.*
 import com.intellij.util.ui.JBUI
-import java.awt.BorderLayout
-import java.awt.FlowLayout
-import java.awt.GridLayout
+import java.awt.*
 import javax.swing.*
-
 
 class PluginConfigurable : Configurable, Configurable.Beta {
     private var settingsPanel: JPanel? = null
@@ -18,89 +14,81 @@ class PluginConfigurable : Configurable, Configurable.Beta {
     private var copyIgnoreFileNamesListModel: DefaultListModel<String>? = null
     private var copyIgnoreFileNamesList: JList<String>? = null
     private var fileNameInputField: JTextField? = null
-    private var fileNameAddButton: JButton? = null
-    private var fileNameRemoveButton: JButton? = null
 
     override fun createComponent(): JComponent? {
-        settingsPanel = JPanel().apply {
-            layout = GridLayout(3, 1)
-        }
+        settingsPanel = JPanel(GridBagLayout())
 
-        val margin = JBUI.insets(5)
+        val gbc = GridBagConstraints()
+        gbc.fill = GridBagConstraints.HORIZONTAL
+        gbc.gridx = 0
+        gbc.weightx = 1.0
+        gbc.anchor = GridBagConstraints.NORTHWEST
 
         val wipDisclaimerTextArea = getDisclaimerSection()
-        val tempProjectDirNameLabeledPanel = getTempProjectDirNameLabeledPanel(margin)
-        val ignoreFileNamesPanel = getCopyIgnoreFileNamesListSection(margin)
+        gbc.gridy = 0
+        settingsPanel?.add(wipDisclaimerTextArea, gbc)
 
-        return settingsPanel?.apply {
-            add(wipDisclaimerTextArea, BorderLayout.PAGE_START)
-            add(tempProjectDirNameLabeledPanel, BorderLayout.CENTER)
-            add(ignoreFileNamesPanel, BorderLayout.CENTER)
-        }
+        val generalSettingsSection = getGeneralSettingsSection()
+        gbc.gridy = 1
+        gbc.insets = JBUI.insetsTop(10)
+        settingsPanel?.add(generalSettingsSection, gbc)
+
+        val ignoreFileNamesPanel = getCopyIgnoreFileNamesListSection()
+        gbc.gridy = 2
+        gbc.insets = JBUI.insetsTop(10)
+        settingsPanel?.add(ignoreFileNamesPanel, gbc)
+
+        gbc.gridy = 3
+        gbc.weighty = 1.0
+        settingsPanel?.add(Box.createVerticalBox(), gbc)
+
+        return settingsPanel
     }
 
-    private fun getTempProjectDirNameLabeledPanel(margin: JBInsets): JComponent {
-        tempProjectDirNameField = JTextField(PluginConfig.getInstance().tempProjectDirName, 20).apply {
-            toolTipText = "Name of the temporary directory to be used by the plugin."
-        }
-
-        return JPanel(FlowLayout()).apply {
-            border = BorderFactory.createCompoundBorder(
-                BorderFactory.createTitledBorder("General settings"),
-                JBUI.Borders.empty(margin.top, margin.left, margin.bottom, margin.right)
-            )
-            add(JLabel("Temp Project Directory Name:"))
-            add(tempProjectDirNameField)
-        }
-    }
-
-    private fun getDisclaimerSection(): JComponent = JBTextArea(2, 40).apply {
-        text = "Note: Settings are still being developed. Functionality might not work as expected."
-        wrapStyleWord = true
-        lineWrap = true
-        isEditable = false
-        isFocusable = false
-        background = UIManager.getColor("Label.background")
-        font = UIManager.getFont("Label.font")
-        border = JBUI.Borders.empty()
-    }
-
-    private fun getCopyIgnoreFileNamesListSection(margin: JBInsets): JComponent {
-        fileNameInputField = JTextField(20).apply {
-            addActionListener { _ ->
-                fileNameRemoveButton?.isEnabled = copyIgnoreFileNamesList?.isSelectionEmpty == false
-                fileNameAddButton?.isEnabled = fileNameInputField?.text?.isNotEmpty() == true
-            }
-            toolTipText = "Enter the name of the file or directory to ignore during copy."
-        }
-
-        fileNameAddButton = JButton("Add").apply {
-            addActionListener { _ ->
-                val textToAdd = fileNameInputField?.text
-                if (textToAdd?.isEmpty() == false && copyIgnoreFileNamesListModel?.contains(textToAdd) == false) {
-                    copyIgnoreFileNamesListModel?.addElement(textToAdd)
-                    fileNameInputField?.text = ""
-                }
-            }
+    private fun getCopyIgnoreFileNamesListSection(): JComponent {
+        val fileNameAddButton = JButton("Add").apply {
             toolTipText = "Add the specified name to the ignore list."
+            addActionListener { _ ->
+                fileNameInputField?.text
+                    ?.takeIf { it.isNotBlank() }
+                    ?.takeIf { copyIgnoreFileNamesListModel?.contains(it) == false }
+                    ?.let {
+                        copyIgnoreFileNamesListModel?.addElement(it)
+                        fileNameInputField?.text = ""
+                    }
+            }
         }
 
-        fileNameRemoveButton = JButton("Remove").apply {
+        val fileNameRemoveButton = JButton("Remove").apply {
             addActionListener { _ ->
-                val selectedIndex: Int = copyIgnoreFileNamesList?.selectedIndex ?: -1
-                if (selectedIndex != -1) {
-                    copyIgnoreFileNamesList?.remove(selectedIndex)
+                copyIgnoreFileNamesList?.selectedIndex?.let { selectedIndex ->
                     copyIgnoreFileNamesListModel?.remove(selectedIndex)
                 }
             }
             toolTipText = "Remove the selected name from the ignore list."
+            isEnabled = false
         }
 
         copyIgnoreFileNamesListModel = DefaultListModel<String>().apply {
             addAll(PluginConfig.getInstance().copyIgnoreFileNames)
         }
 
-        copyIgnoreFileNamesList = JBList(*PluginConfig.getInstance().copyIgnoreFileNames.toTypedArray())
+        copyIgnoreFileNamesList = JBList(*PluginConfig.getInstance().copyIgnoreFileNames.toTypedArray()).apply {
+            selectionMode = ListSelectionModel.SINGLE_SELECTION
+            layoutOrientation = JList.VERTICAL
+            visibleRowCount = 7
+            model = copyIgnoreFileNamesListModel
+            addListSelectionListener { event ->
+                if (!event.valueIsAdjusting) {
+                    fileNameRemoveButton.isEnabled = copyIgnoreFileNamesList?.isSelectionEmpty != true
+                }
+            }
+        }
+
+        fileNameInputField = JTextField(20).apply {
+            toolTipText = "Enter the name of the file or directory to ignore during copy."
+            addActionListener { fileNameAddButton.isEnabled = text.isNotBlank() }
+        }
 
         val inputPanel = JPanel(FlowLayout()).apply {
             add(fileNameInputField)
@@ -108,17 +96,65 @@ class PluginConfigurable : Configurable, Configurable.Beta {
             add(fileNameRemoveButton)
         }
 
-        return JPanel().apply {
-            layout = BoxLayout(this, BoxLayout.PAGE_AXIS)
-            border = JBUI.Borders.compound(
-                BorderFactory.createTitledBorder("Copy Ignore List"),
-                JBUI.Borders.empty(margin.top, margin.left, margin.bottom, margin.right)
-            )
-            add(inputPanel)
-            add(
-                JBScrollPane(copyIgnoreFileNamesList)
-            )
+        return JPanel(GridBagLayout()).apply {
+            border = JBUI.Borders.empty(5, 0)
+
+            val gbc = GridBagConstraints()
+            gbc.fill = GridBagConstraints.HORIZONTAL
+            gbc.weightx = 1.0
+            gbc.gridx = 0
+
+            gbc.gridy = 0
+            add(TitledSeparator("Copy Ignore List"), gbc)
+
+            gbc.gridy = 1
+            gbc.weighty = 0.0
+            add(inputPanel, gbc)
+
+            gbc.gridy = 2
+            gbc.weighty = 1.0
+            add(JBScrollPane(copyIgnoreFileNamesList), gbc)
         }
+    }
+
+    private fun getGeneralSettingsSection(): JComponent {
+        tempProjectDirNameField = JBTextField(PluginConfig.getInstance().tempProjectDirName, 20).apply {
+            toolTipText = "Name of the temporary directory to be used by the plugin."
+        }
+
+        val tempProjectDirNameLabeledField = JPanel(FlowLayout(FlowLayout.LEADING)).apply {
+            border = JBUI.Borders.empty(5, 0)
+            add(JLabel("Temp Project Directory Name:"))
+            add(tempProjectDirNameField)
+        }
+
+        return JPanel(GridBagLayout()).apply {
+            border = JBUI.Borders.empty(5, 0)
+
+            val gbc = GridBagConstraints()
+            gbc.fill = GridBagConstraints.HORIZONTAL
+            gbc.weightx = 1.0
+            gbc.gridx = 0
+
+            gbc.gridy = 0
+            add(TitledSeparator("General Settings", tempProjectDirNameLabeledField), gbc)
+
+            gbc.gridy = 1
+            gbc.weighty = 0.0
+            add(tempProjectDirNameLabeledField, gbc)
+        }
+    }
+
+    private fun getDisclaimerSection(): JComponent = JBTextArea(1, 40).apply {
+        text = "Note: Settings are still being developed. Functionality might not work as expected."
+        wrapStyleWord = true
+        lineWrap = true
+        isEditable = false
+        isFocusable = false
+        foreground = JBColor.GRAY
+        background = UIManager.getColor("Label.background")
+        font = UIManager.getFont("Label.font")
+        border = JBUI.Borders.empty()
     }
 
     override fun isModified(): Boolean = PluginConfig.getInstance().let { settings ->
